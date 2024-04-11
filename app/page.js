61d315486436c5f37abec990ead1be324d26fe87
navigareweb <web@navigaresrl.com>
2024-04-11 10:44:41 +0200

    first commit

--- app/page.js
+++ app/page.js
@@ -1,94 +1,34 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./index.module.scss";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Image
+        src={"/400x600.jpeg"}
+        width={600}
+        height={600}
+        className={styles.img}
+        alt="imgLautrec"
+      ></Image>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
+        <h3 className={styles.text}>LE RIRE</h3>
+        <audio controls src="/LE-RIRE.mp3" className={styles.audio}>
+          Your browser does not support the
+          <code>audio</code> element.
+        </audio>
+        <p className={styles.p}>
+          Le Rire was a French magazine of political and social satire founded
+          in Paris by Felix Juven and published from October 1894 to 1950. Le
+          Rire was one of the newspaper hosting major artists of the time,
+          including Théophile Steinlen, Henri de Toulouse-Lautrec, and
+          Jean-Louis Forain. It also launched important authors and drawers,
+          such as the French-Spanish Manuel Luque, who was also an Impressionist
+          painter, Adolphe Willette, drawer and engraver, René Georges
+          Hermann-Paul, Juan Gris, later a Cubist painter, Lucien Metivet,
+          Georges Meunier, Joaquín Xaudaró, Leonetto Cappiello, Albert
+          Guillaume, Jules Grandjouan, and Jules-Alexandre Grün.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
